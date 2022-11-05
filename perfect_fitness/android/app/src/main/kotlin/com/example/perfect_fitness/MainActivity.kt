430996c3a99ac8e6b650d4b3e7e7d5eaa6a5d641
Wilton Santhosh D Souza <wiltondsouza100@gmail.com>
2022-11-05 10:58:34 +0530

    initial commit of the codes

---
+++ perfect_fitness/android/app/src/main/kotlin/com/example/perfect_fitness/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.perfect_fitness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ perfect_fitness/lib/core/const/global_constants.dart
@@ -0,0 +1,7 @@
+import 'package:perfect_fitness/data/user_data.dart';
+import 'package:perfect_fitness/data/workout_data.dart';
+
+class GlobalConstants {
+  static late UserData currentUser;
+  static late List<WorkoutData> workouts;
+}
